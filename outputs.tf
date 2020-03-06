db740b36bfab7b0893b7f66f0e90fa977267ade4
FabiusE <fabius.engel@msg.group>
2020-03-06 12:47:19 +0100

    Resource Group as Output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "ip_address" {
 output "hostname" {
   value = azurerm_public_ip.public_ip.fqdn
 }
+
+output "dns_resource_group" {
+  value = azurerm_resource_group.dns_resource_group.name
+}
