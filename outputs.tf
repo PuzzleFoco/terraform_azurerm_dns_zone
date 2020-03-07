12df71d02a41d653cca13312558bc7f5f97454c2
FabiusE <fabius.engel@msg.group>
2020-03-07 17:32:08 +0100

    Output root_domain

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,14 @@ output "ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
 
-output "hostname" {
+output "fqdn" {
   value = azurerm_public_ip.public_ip.fqdn
 }
 
 output "dns_resource_group" {
   value = azurerm_resource_group.dns_resource_group.name
+}
+
+output "root_domain" {
+  value = var.root_domain
 }
